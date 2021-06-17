becdb379f6493c6f43481ece19575a2092e395c9
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-06-17 17:02:03 +0530

    Made the View Model class and setted up some more dagger hilt functions

--- app/src/main/java/com/example/currencyconverter/di/AppModule.kt
+++ app/src/main/java/com/example/currencyconverter/di/AppModule.kt
@@ -2,10 +2,15 @@ package com.example.currencyconverter.di
 
 import android.app.Application
 import com.example.currencyconverter.data.CurrencyApi
+import com.example.currencyconverter.main.DefaultMainRepository
+import com.example.currencyconverter.main.MainRepository
+import com.example.currencyconverter.util.DispatcherProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -26,6 +31,23 @@ object AppModule {
         .build()
         .create(CurrencyApi::class.java)
 
+
+    @Singleton
+    @Provides
+    fun providesMainRepository(api : CurrencyApi): MainRepository = DefaultMainRepository(api)
+
+    @Singleton
+    @Provides
+    fun providesDispatchers(): DispatcherProvider = object : DispatcherProvider{
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+    }
 
 
 
